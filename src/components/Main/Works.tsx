016ccb81ab8b34686b894672dc5a888658e1a5cc
Rafael Melo <rsmelo_@hotmail.com>
2024-01-19 12:14:27 -0300

    chore: remove unused

--- src/components/Main/Works.tsx
+++ src/components/Main/Works.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx"
 import squares from "/works/squares.webp"
 import silhouette from "/works/silhouette.jpg"
 import tetris from "/works/tetris.gif"
@@ -15,7 +16,6 @@ import architect from "/works/architect.gif"
 import placeholder from "/works/placeholder.jpg"
 
 import classes from './Works.module.css'
-import clsx from "clsx"
 
 type Props = { 
   title?: string
@@ -28,166 +28,136 @@ export const Works = ({ title }: Props) => {
       <div className={classes.grid}>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${architect})`,
-              backgroundSize: "cover",
               backgroundPosition: "center 52%",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={clsx(classes.forceDark, classes.box)}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${DS})`,
-              backgroundSize: "cover",
-              backgroundPosition:"center",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${placeholder})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={clsx(classes.forceDark, classes.box)}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${surfe})`,
-              backgroundSize: "cover",
               filter: "invert(100%) contrast(0.9) brightness(0.666)",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${consultar})`,
-              backgroundSize: "cover",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${lesgow})`,
-              backgroundSize: "cover",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${silhouette})`,
-              backgroundSize: "cover",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${solissolutions})`,
               backgroundSize: "contain",
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
               backgroundColor: "#0b1319",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${student})`,
-              backgroundSize: "cover",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={clsx(classes.forceDark, classes.box)}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${lula})`,
-              backgroundSize: "cover",
-              width: "100%",
               filter: "invert(100%) contrast(0.9) brightness(0.666)",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${bilheteriapp})`,
-              backgroundSize: "cover",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={clsx(classes.forceDark, classes.box)}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${onlineoffice})`,
               backgroundSize: "contain",
-              filter: "invert(100%) contrast(0.9) brightness(0.666)",
               backgroundPosition:"center",
-              width: "100%",
-              height: "100%",
+              filter: "invert(100%) contrast(0.9) brightness(0.666)",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${squares})`,
-              backgroundSize: "cover",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={clsx(classes.forceDark, classes.box)}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${ooapp})`,
               backgroundSize: "contain",
-              backgroundRepeat:"no-repeat",
+              backgroundPosition: "center",
               backgroundColor: "white",
               filter: "invert(100%) contrast(0.9) brightness(0.666)",
-              backgroundPosition: "center",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
         <div className={classes.box}>
           <div
+            className={classes.boxContent}
             style={{
               backgroundImage:`url(${tetris})`,
-              backgroundSize: "cover",
               filter: "invert(100%) contrast(0.9) brightness(0.666)",
-              width: "100%",
-              height: "100%",
             }} 
           />
         </div>
