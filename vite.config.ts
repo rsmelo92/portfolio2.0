4b58d26be558f182612725bc0c2d03469df1bb0b
Rafael Melo <rsmelo_@hotmail.com>
2024-01-19 13:59:10 -0300

    chore: use repository name on vite.base and clean readme

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/portfolio2.0/",
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
